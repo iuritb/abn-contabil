69d6751a532e57637037cf3561f618a1856980a8
iuri.barros <iuri.t.barros@gmail.com>
2023-11-09 15:54:50 -0300

    chore: create navbar

--- src/components/ui/navbar.vue
+++ src/components/ui/navbar.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="rectangle-29-rectangle-29x"></div>
+  <div class="navbar">
+    <div class="navbar__text">
+      <div class="navbar__text-item">Receita Federal</div>
+      <div class="navbar__text-item">Meu INSS</div>
+      <div class="navbar__text-item">ISS Online</div>
+      <div class="navbar__text-item">ISS Net Online</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,10 +14,27 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.rectangle-29-rectangle-29x {
-  background-color: rgba(224, 224, 224, 1);
+p {
+  margin: 0;
+}
+.navbar {
+  background-color: #e0e0e0;
   box-sizing: border-box;
   height: 96px;
   width: 100%;
+  &__text {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+  }
+  &__text-item {
+    margin-top: 35px;
+    color: #4f4f4f;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 25.2px; /* 140% */
+  }
 }
 </style>
