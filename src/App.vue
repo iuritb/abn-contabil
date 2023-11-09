8014663c5bd95acdc3fb167340c7edb5fc34d5c7
iuri.barros <iuri.t.barros@gmail.com>
2023-11-09 15:33:57 -0300

    chore: change font

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
